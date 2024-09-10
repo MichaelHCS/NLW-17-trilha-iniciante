fc9ad02e8ec587d8acfb03e39597ab5f294ab6a1
MichaelHCS <michaelhenriquecs25@gmail.com>
2024-09-10 13:45:44 -0300

    adiciona inquire

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-console.log("Hello FDP") // CONST = nao permite alteração da variavel depois de ja definida e LET = permite a alteração depois de difinida 
-                        // dentro de {} sao variaveis LOCAIS e fora são definidas como GLOBAIS
-                        // COmando RELOAD WINDOWNS reset o vs code
+// ESTRUTURA DE REPETIÇÃO DENTRO DE UMA ARROW FUNCTION
+const start = () => {
+
+    while(true){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "Listar":
+                console.log("vamos lIstar")
+                break
+            case "sair":
+                return
+
+        }    
+    }
+}
+
+
+
+start()
+
+
+
+
+
+
+
+
+//console.log("Hello FDP") 
+// CONST = nao permite alteração da variavel depois de ja definida e LET = permite a alteração depois de difinida 
+// dentro de {} sao variaveis LOCAIS e fora são definidas como GLOBAIS
+// COmando RELOAD WINDOWNS reset o vs code
+
+//let = metas = ["Alô", "Michael"]
+
+//console.log(metas[1] + " " + metas[0])
+
+
+//let meta = {
+  //  value: 'Ler um livro por mês',
+    //checked: false,
+//}
+
+//let metas = [
+    //meta,
+   // {
+    //    value: "caminhar 20 minutos todos os dias",
+      //  checked: false    
+  //  }
+//]
+
+//console.log(metas[1].value)
