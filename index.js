21c7ce0fc18a8426b4574cc1abb7449cff1ff992
MichaelHCS <michaelhenriquecs25@gmail.com>
2024-09-10 08:14:02 -0300

    Fundamentos: 001

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello FDP") // CONST = nao permite alteração da variavel depois de ja definida e LET = permite a alteração depois de difinida 
+                        // dentro de {} sao variaveis LOCAIS e fora são definidas como GLOBAIS
+                        // COmando RELOAD WINDOWNS reset o vs code
