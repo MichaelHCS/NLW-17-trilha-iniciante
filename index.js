c09d1472970cad40fe4ea4d14fadf5e1f6dfa905
MichaelHCS <michaelhenriquecs25@gmail.com>
2024-09-11 19:30:56 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -1,29 +1,111 @@
 // ESTRUTURA DE REPETIÇÃO DENTRO DE UMA ARROW FUNCTION
-const {select} = require('@inquirer/prompts')
+const {select, input, checkbox} = require('@inquirer/prompts')
 
-const start = async () => {
+
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,  
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input ({ message: "Digite a meta"})
+
+    if (meta.length == 0) {
+        console.log("A meta nao pode ser vazia")
+        return
+    }
+
+    metas.push(
+        {value: meta, checked: false}
+    )
+
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices:[...metas],
+        instructions: false,
+    })
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((respostas) =>{
+        const meta = metas.find((m) => {
+            return m.value == respostas
+        })
+        
+        meta.checked =true
+    })
+    console.log('Meta(s) marcadas como concluída(s)')
+}
+
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+
+    })
+    if(realizadas.length == 0){
+        console.log('Não existem metas realizadas! :(')
+        return
+    }
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+}
+
+const start = async() => {
 
     while(true){
-       
-
+    
         const opcao = await select({
+        message: "Menu >",
+        choices: [
+            {
+                name: "Cadastrar Meta",
+                value: "Cadastrar"
+            },
+            {
+                name: "Listar Metas",
+                value: "Listar"
+            },
+            {
+                name: "Metas Realizadas",
+                value: "realizadas"
+            },
+            {
+                name: "Sair",
+                value: "Sair"
 
-
-
-
-        })
-
-
-
+            }
+        ]
+    })
 
         switch(opcao){
             case "cadastrar":
-                console.log("Vamos cadastrar")
+            await cadastrarMeta()   
+            console.log(metas)
                 break
+
             case "Listar":
-                console.log("vamos lIstar")
+                await listarMetas()
                 break
+
+            case "realizadas":
+                await metasRealizadas()
+                break
+
             case "sair":
+                console.log("Até a próxima")
                 return
 
         }    
